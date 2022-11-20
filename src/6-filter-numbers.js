00e37d1f05c47a48a462cb6537210c78c4741c03
Archkrull <102250838+Archkrull@users.noreply.github.com>
2022-11-20 12:53:16 +0100

    practice

--- src/6-filter-numbers.js
+++ src/6-filter-numbers.js
@@ -1,3 +1,11 @@
 export const filterNumbers = (array, largerThan) => {
-  return array
-}
+  
+  }
+
+
+const numbers = [20, 40, 17, 99, 59, 77];
+const filteredNumbers = numbers.filter((number) => {
+    return number > 20;
+});
+  
+console.log(filteredNumbers); // [40,99,59,77]
